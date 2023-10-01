832b40800f961017a33a822c7906f98d50f3d0a4
Omkar <goa11@iitbbs.ac.in>
2023-10-01 17:28:15 +0530

    feat: validate only reply author can patch/delete

--- routes/replyRoutes.js
+++ routes/replyRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const { postReply, getReplies, getReplyById, patchReply, getReplyThread, deleteReply } = require("../controllers/replyController.js");
-const { protect } = require("../middleware/authMiddleware.js");
-
+const {
+  postReply,
+  getReplies,
+  getReplyById,
+  patchReply,
+  getReplyThread,
+  deleteReply,
+} = require("../controllers/replyController.js");
+const { protect, verifyCreator } = require("../middleware/authMiddleware.js");
+const Reply = require("../models/replymodel");
 const replyRouter = express.Router();
 
 // Features
@@ -10,10 +17,12 @@ const replyRouter = express.Router();
 // 3. view all replies to a post
 // 4. view all replies to a reply
 
-// TODO: add a validator that only reply author can patch and delete replies
 replyRouter.route("/").get(getReplies).post(protect, postReply);
-replyRouter.route("/:id").get(getReplyById).patch(protect, patchReply).delete(deleteReply)
-replyRouter.route("/:id/thread").get(getReplyThread)
-
+replyRouter
+  .route("/:id")
+  .get(getReplyById)
+  .patch(protect, verifyCreator(Reply), patchReply)
+  .delete(protect, verifyCreator(Reply), deleteReply);
+replyRouter.route("/:id/thread").get(getReplyThread);
 
 module.exports = replyRouter;
